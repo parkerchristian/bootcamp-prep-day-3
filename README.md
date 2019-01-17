b06aad41ba7f01080968d4d3e54fb0351e02d5c6
Parker Helland <hellandp@gmail.com>
2019-01-16 20:42:25 -0800

    newest changes to JavaScript page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bootcamp-prep-day-3
 JavaScript FTW!
+
+Note: We set '.eslintrc' 'no-console' to "off"
--- main.js
+++ main.js
@@ -1,2 +1,42 @@
-var sum = 3 + 4;
-console.log('hello console!', sum);
+var x = 15;
+
+if(x > 12) {
+    console.log(x);
+    console.log('groovin with if')
+}
+else {
+    console.log('too small');
+    ;
+}
+
+// 1. Initialize
+// 2. Condition
+// 3. Post Loop Action
+for(var i = 0; i < x; i++)   {
+    if(i % 2 === 0) {
+    console.log(i);
+
+    }
+}
+
+function sayHello() {
+    console.log('hi')
+}
+if(x % 2 === 0){
+    sayHello();
+}
+
+// eslint-disable-next-line
+function evenOrOdd() {
+    var input = document.getElementById('number'); 
+    
+    var y = input.value;
+
+    if(y % 2 === 0){
+        console.log('even')
+    }
+
+    else {
+        console.log('odd')
+    }
+}
